017d52bd4b0aeb79cac0005e216c2a3f70504a28
Isaías Reyes León <elreyieon@Isaiass-MacBook-Air.local>
2022-02-22 13:08:43 -0600

    Added routes for user dashboards, posting new tips and also for the homeroutes. Added seed information for the DB and the homepage handlebars to display the posts list.

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,43 @@
+const router = require('express').Router();
+const { post } = require('../../../01-Activities/28-Stu_Mini-Project/Main/controllers/api/projectRoutes');
+const { Post } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+
+// Route to create new post
+router.post('/', withAuth, async (req, req) => {
+    try {
+        const newPost = await Post.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+
+// Route to delete specific post
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with this id!'});
+            return;
+        }
+
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
---
+++ controllers/homeRoutes.js
@@ -0,0 +1,83 @@
+const router = require('express').Router();
+const { truncate } = require('lodash');
+const { userInfo } = require('os');
+const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+
+// Route for homepage
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['title'],
+                },
+            ],
+        });
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        res.render('homepage', {
+            posts,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Route for specific post
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: userInfo,
+                    attributes: ['title'],
+                },
+            ],
+        });
+
+        const post = postData.get({ plain: true });
+
+        res.render('post', {
+            ...post,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Route for user dashboard
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: Post }],
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render('dashboard', {
+            ...user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Route for login 
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+
+    res.render('login');
+});
+
+module.exports = router;
